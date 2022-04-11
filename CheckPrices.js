0e358ed983528d3cc8543cc7464d6bbbbb5b5678
Aires <joaoaires98@gmail.com>
2022-04-11 23:24:32 +0100

    the start

--- CheckPrices.js
+++ CheckPrices.js
@@ -6,11 +6,12 @@ const { log } = require('console');
 
 class CheckPrices{
     constructor(){
-        this.games = fs.readFileSync('./input.txt').toString().split(' ').join('+').split("\n");
+        this.gamesInput = fs.readFileSync('./input.txt').toString().split(' ').join('+').split("\n");
         this.urls =[
-            {name: 'jogonamesa' ,url:"https://jogonamesa.pt/P/search.cgi?search=" },
+            {name: 'jogonamesa' ,url:"https://jogonamesa.pt/P/search.cgi?&ordem=nome&search=" },
             {name: 'arenaporto' ,url:"https://arenaporto.com/pesquisa?controller=search&s=" },
         ];
+        this.gamesToPrint = [];
     }
 
     async scrape(url){
@@ -23,38 +24,48 @@ class CheckPrices{
     }
 
     async getData(){
-        // for (let i = 0; i < this.games.length; i++) {
-            // let game = this.games[i];
-            let game = this.games[0];
+        // for (let i = 0; i < this.gamesInput.length; i++) {
+            // let game = this.gamesInput[i];
+            let game = this.gamesInput[0];
             console.log("Starting with game: ", game);
             for (let j = 0; j < this.urls.length; j++) {
                 let site = this.urls[j].name;
+
                 console.log("Game: " + game + "; Site:" + site);
+
                 let url = this.urls[j].url;
                 url += game;
                 let html = await this.scrape(url);
                 let $ = cheerio.load(html);
                 switch (site) {
                     case "jogonamesa":
-                        let listItems = $(".lista_items");
-                        listItems.each(function (idx, el) {
-                            
-                            console.log($(el).text());
-                        });
+                        this.jogonamesa($);
                         break;
                 }
+                console.log(this.gamesToPrint);
             }
         // }
 
-        // html = await this.scrape(this.urls[0].url);
-        // let $ = cheerio.load(html);
-        // let listItems = $(".lista_items");
+    }
 
-        // listItems.each(function (idx, el) {
-        //     console.log($(el).text());
-        // });
+    jogonamesa($){
+        let listItems = $(".lista_items a");
+        listItems.each( (idx, el) =>{
+            if(idx == 3) return false;
+            let name = $(el).find(".item_nome").text();
+            let price = $(el).find(".item_preco").text();
+            let cheaperPrice = $(el).find(".item_preco_barato").text();
+            let stock = $(el).find(".item_disponibilidade").text();
+
+            console.log(idx);
+            this.gamesToPrint.push({name: name, price: price, cheaperPrice: cheaperPrice, stock: stock});
+            // console.log({name: name, price: price, cheaperPrice: cheaperPrice, stock: stock});
+            // console.log(`${stock}\n`);
+        });
     }
 }
+
+
 
 let price = new CheckPrices();
 // console.log(price.testURl);
