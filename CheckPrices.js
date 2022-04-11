0d86e8b0cbfe811cfcef1e18964a2f4b1c3ed388
João Aires <>
2022-04-11 16:32:02 +0100

    update

--- CheckPrices.js
+++ CheckPrices.js
@@ -4,22 +4,27 @@ const fetch = require("node-fetch");
 class CheckPrices{
     constructor(){
         this.testURl = 'https://jogonamesa.pt/P/search.cgi?search=champions+of+midgard';
-        this.testURl = 'https://arenaporto.com/pesquisa?controller=search&s=roll+for+the+galaxy';
+        // this.testURl = 'https://arenaporto.com/pesquisa?controller=search&s=roll+for+the+galaxy';
 
-        // let htmlparser2 = require('htmlparser2');
-        // let dom = htmlparser2.parseDocument(document, options);
     }
 
-    async getData(){
+    async scrape(url){
         // const response = fetch(url);
         // let text = await response.text();
         // console.log(text);
 
-        // let response = await fetch(this.testURl);
-        // let html = await response.text();
-        // console.log(html);
+        let response = await fetch(url);
+        return await response.text();
+    }
 
-        cheerio.load(this.testURl);
+    async getData(){
+        let html = await this.scrape(this.testURl);
+        let $ = cheerio.load(html);
+        let listItems = $(".lista_items");
+
+        listItems.each(function (idx, el) {
+            console.log($(el).text());
+          });
     }
 }
 
