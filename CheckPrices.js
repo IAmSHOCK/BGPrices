1a77c406317de0c02413b42feb89c004bf24cde4
João Aires <>
2022-04-11 14:50:25 +0100

    it returns the HTMLnode checkPrices.js

--- CheckPrices.js
+++ CheckPrices.js
@@ -3,20 +3,23 @@ const fetch = require("node-fetch");
 
 class CheckPrices{
     constructor(){
-        let testURl = "https://jogonamesa.pt/P/ficha.cgi?bgg_id=172287";
+        this.testURl = 'https://jogonamesa.pt/P/ficha.cgi?bgg_id=172287';
+
         // let htmlparser2 = require('htmlparser2');
         // let dom = htmlparser2.parseDocument(document, options);
     }
 
-    getData(url){
+    getData(){
         // const response = fetch(url);
         // let text = await response.text();
         // console.log(text);
 
-        return fetch.fetch(url)
+        fetch(this.testURl)
             .then(res => res.text())
             .then(text => console.log(text));
     }
 }
 
-module.exports.CheckPrices = { CheckPrices };
+let price = new CheckPrices();
+// console.log(price.testURl);
+price.getData();
