a03193ee907b86222a19a44cf4e1e6e8aee51c54
João Aires <>
2022-04-11 18:01:08 +0100

    update

--- CheckPrices.js
+++ CheckPrices.js
@@ -1,11 +1,16 @@
 const cheerio = require('cheerio');
 const fetch = require("node-fetch");
+const fs = require("fs");
+const { log } = require('console');
+
 
 class CheckPrices{
     constructor(){
-        this.testURl = 'https://jogonamesa.pt/P/search.cgi?search=champions+of+midgard';
-        // this.testURl = 'https://arenaporto.com/pesquisa?controller=search&s=roll+for+the+galaxy';
-
+        this.games = fs.readFileSync('./input.txt').toString().split(' ').join('+').split("\n");
+        this.urls =[
+            {name: 'jogonamesa' ,url:"https://jogonamesa.pt/P/search.cgi?search=" },
+            {name: 'arenaporto' ,url:"https://arenaporto.com/pesquisa?controller=search&s=" },
+        ];
     }
 
     async scrape(url){
@@ -18,13 +23,36 @@ class CheckPrices{
     }
 
     async getData(){
-        let html = await this.scrape(this.testURl);
-        let $ = cheerio.load(html);
-        let listItems = $(".lista_items");
+        // for (let i = 0; i < this.games.length; i++) {
+            // let game = this.games[i];
+            let game = this.games[0];
+            console.log("Starting with game: ", game);
+            for (let j = 0; j < this.urls.length; j++) {
+                let site = this.urls[j].name;
+                console.log("Game: " + game + "; Site:" + site);
+                let url = this.urls[j].url;
+                url += game;
+                let html = await this.scrape(url);
+                let $ = cheerio.load(html);
+                switch (site) {
+                    case "jogonamesa":
+                        let listItems = $(".lista_items");
+                        listItems.each(function (idx, el) {
+                            
+                            console.log($(el).text());
+                        });
+                        break;
+                }
+            }
+        // }
 
-        listItems.each(function (idx, el) {
-            console.log($(el).text());
-          });
+        // html = await this.scrape(this.urls[0].url);
+        // let $ = cheerio.load(html);
+        // let listItems = $(".lista_items");
+
+        // listItems.each(function (idx, el) {
+        //     console.log($(el).text());
+        // });
     }
 }
 
