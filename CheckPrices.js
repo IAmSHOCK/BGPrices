54892a5e6ea4d2c857372c0146f18ebecbb08d44
João Aires <>
2022-04-14 00:59:02 +0100

    update

--- CheckPrices.js
+++ CheckPrices.js
@@ -1,52 +1,73 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 
+//sitename = FRUKLITS
 
 class CheckPrices{
     constructor(){
         this.gamesInput = fs.readFileSync('./input.txt').toString().split("\n");
     }
 
-    async scrape(){
-
+    scrape(){
         // {gameName: string, data: [{ store: string, price: (int?), availability: string }] }
-        let browser = await puppeteer.launch();
-        this.page = await browser.newPage();
-        this.gamesToPrint = [];
-        let gameName = "";
+        this.scrapedGames = [];
+
+        let gameName = this.gamesInput[0];
+        let price = 0;
+        let stock = "";
+
         let prices = [];
         let j = 0;
-        for (let i = 0; i < this.gamesInput.length; i++) {
+        let k = 0;
+        //i is for input, j is for stores of a given game and k is for scrapedGames
+        for (let i = 1; i < this.gamesInput.length; i++) {
             let elem = this.gamesInput[i];
             if (this.isGame(elem)){
-
+                let tmp = {gameName: gameName, data: prices};
+                console.log("Final scraped game: ", tmp);
+                this.scrapedGames[k++] = tmp;
                 gameName = elem;
+                console.log("Game name: ", gameName);
+                prices = [];
                 j = 0;
             }
             else {
+                let obj = {};
+                let hostName = this.getHostName(elem);
+                console.log("hostname: ", hostName);
                 switch(elem){
-
+                    case "jogonames.pt":
+                        this.jogonamesa(elem);
                 }
 
-                urls[j++] =  elem;
+                // obj = { store: string, price: (int?), availability: string }
+                console.log("obj: ", obj);
+                prices[j++] =  obj;
             }
-            if(i == 3)
-            // console.log("elem: ", elem);
             if(i == 7) break;
         }
 
         console.log("Closing browser.");
-        browser.close();
+    }
+
+    getHostName(elem){
+        return new URL(elem).hostname;
     }
 
     isGame(elem){
-        return true;
+        return !elem.includes("http");
     }
 
-    jogonamesa(){
+    async jogonamesa(url){
+        let browser = await puppeteer.launch();
+        this.page = await browser.newPage();
+
+        browser.close();
     }
 
-
+    getOldPrices(){
+        return "TODO";
+    }
 }
 
 
