bbf2351af937e78d7a8376f8099a0d3c7c6d8a41
João Aires <>
2022-04-13 21:33:18 +0100

    update

--- CheckPrices.js
+++ CheckPrices.js
@@ -4,22 +4,44 @@ const fs = require("fs");
 
 class CheckPrices{
     constructor(){
+        this.gamesInput = fs.readFileSync('./input.txt').toString().split("\n");
     }
 
-    async init(){
-        this.gamesInput = fs.readFileSync('./input.txt').toString().split(' ').join('+').split("\n");
+    async scrape(){
+        let browser = await puppeteer.launch();
+        this.page = await browser.newPage();
         this.gamesToPrint = [];
-        this.browser = await puppeteer.launch();
-        this.page = await this.browser.newPage();
+        let obj = {};
+        for (let i = 0; i < this.gamesInput.length; i++) {
+            let elem = this.gamesInput[i];
+            if (this.isGame(elem)){
+
+                obj = {name: elem}
+            }
+            else {
+
+            }
+            console.log("elem: ", elem);
+            if(i == 7) break;
+        }
+
+        console.log("Closing browser.");
+        browser.close();
     }
 
-    jogonamesa($){
+    isGame(elem){
+        return true;
     }
+
+    jogonamesa(){
+    }
+
+
 }
 
 
 
 let price = new CheckPrices();
-price.init();
+price.scrape();
 // console.log(price.testURl);
 // price.getData();
